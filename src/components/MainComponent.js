b67074f768e6674c97067b7e1e2d1af7c7bad51b
Amos Ahounou <amosahounou@gmail.com>
2021-04-27 12:34:36 +0100

    react Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,11 @@
 import React,{ Component } from 'react' ;
+import Home from "./HomeComponent";
 import Menu from './MenuComponent';
 import Detaildish from "./DetailsDishComponent";
 import Header from "./HeadersComponent";
 import Footer from "./FooterComponent";
 import { DISHES } from '../shared/dishes';
-
+import  {Switch,Route,Redirect} from "react-router-dom";
 
 
 /** on a import Menu de MEnu component
@@ -15,28 +16,35 @@ fais en bas dans MenuComponent
  filter filtre le tableau selon la condition et retourne un sous
  tableau avecle element corespondant au filtrage(condition)
  etant un tableau le"[0]" veut dire recuperer le
- premeier element du tableau*/
+ premeier element du tableau
+ Route lorsqu'on ecrit component={Menu} alors on ne peut pas
+ passer des accesoires au composant menu pour passer des accessoires
+ on doit faire component={() => <Menu preciser l'acessoire a ajouter>}
+ Redirect pour une route par defaut*/
 
 class Main extends Component{
     constructor(props) {
         super(props);
         this.state = {
             dishes: DISHES,
-            selectedDish:null
+
         };
     }
-    onDishSelect(dishId) {
-        this.setState({ selectedDish: dishId}
-        );
-    }
+
     render() {
+        const HomePage= () => {
+            return(
+                <Home/>
+            );
+        }
         return (
             <div >
                 <Header/>
-                <Menu dishes={this.state.dishes}
-                onClick={(dishId) => this.onDishSelect(dishId)}/>
-                <Detaildish
-                    dish={this.state.dishes.filter(dish => dish.id === this.state.selectedDish )[0]} />
+                <Switch>
+                    <Route path="/home" component={HomePage}/>
+                    <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Redirect to="/home" />
+                </Switch>
                 <Footer/>
             </div>
         );
